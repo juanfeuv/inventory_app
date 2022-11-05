a42265794202443095276c2c72e3ff172db589f0
juan usuga <jusugav@unal.edu.co>
2022-11-05 09:07:22 -0500

    temporal

--- server/main.js
+++ server/main.js
@@ -1,6 +1,25 @@
 import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
 
 import '../imports/api/methods';
 
 
-Meteor.startup(async () => { });
+Meteor.startup(async () => {
+  // build user admin
+  Accounts.createUser({
+    username: 'admin',
+    password: 'admin12345',
+    profile: {
+      isAdmin: true,
+    },
+  });
+
+  // build user visitor
+  Accounts.createUser({
+    username: 'visitor',
+    password: 'visitor12345',
+    profile: {
+      isAdmin: false,
+    },
+  });
+});
