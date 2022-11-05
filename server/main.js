42b125029d683bd7618dbc7fdd9811b5fbecf059
juan usuga <jusugav@unal.edu.co>
2022-11-05 14:34:55 -0500

    startup

--- server/main.js
+++ server/main.js
@@ -1,25 +1,6 @@
 import { Meteor } from 'meteor/meteor';
-import { Accounts } from 'meteor/accounts-base';
 
 import '../imports/api/methods';
 
 
-Meteor.startup(async () => {
-  // build user admin
-  Accounts.createUser({
-    username: 'admin',
-    password: 'admin12345',
-    profile: {
-      isAdmin: true,
-    },
-  });
-
-  // build user visitor
-  Accounts.createUser({
-    username: 'visitor',
-    password: 'visitor12345',
-    profile: {
-      isAdmin: false,
-    },
-  });
-});
+Meteor.startup(async () => { });
